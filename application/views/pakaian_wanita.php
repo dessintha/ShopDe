f7abc4d2b60c5b02e4e00670f197858ecd479e7f
dessintha <sinthampk@gmail.com>
2020-10-23 15:02:41 +0700

    pakaian_wanita

--- application/views/pakaian_wanita.php
+++ application/views/pakaian_wanita.php
@@ -1,19 +1,19 @@
 <div class="container-fluid">
   <!-- Breadcrumbs-->
   <ol class="breadcrumb">
-    <h4 class="breadcrumb-item"><?php echo $title ?></h4>
+    <h4 class="breadcrumb-item"><?php echo esc_html($title); ?></h4>
   </ol>
 
   <div class="row ml-1 text-center mt-3 mb-3" >
     <?php foreach ($pakaian_wanita as $brg) : ?>
       <div class="card ml-3" style="width: 15rem;">
-        <img src="<?php echo base_url().'/upload/'.$brg->gambar ?>" class="card-img-top" alt="..." >
+        <img src="<?php echo esc_url().'/upload/'.$brg->gambar ?>" class="card-img-top" alt="..." >
         <div class="card-body">
-          <h5 class="card-title"><?php echo $brg->nama_brg ?></h5>
-          <small><?php echo $brg->keterangan ?></small><br>
+          <h5 class="card-title"><?php echo esc_html($brg->nama_brg); ?></h5>
+          <small><?php echo esc_html($brg->keterangan); ?></small><br>
           <span class="badge badge-pill badge-success mb-2">Rp.<?php echo number_format($brg->harga,0,',','.')  ?></span>
-          <?php echo anchor('Dashboard/tambah_keranjang/'.$brg->id_barang, '<div class="btn btn-sm btn-primary">Tambah ke Keranjang</div>') ?>
-          <?php echo anchor('Dashboard/detail/'.$brg->id_barang, '<div class="btn btn-sm btn-success mt-3">Detail</div>') ?>
+          <?php echo esc_url('Dashboard/tambah_keranjang/'.$brg->id_barang, '<div class="btn btn-sm btn-primary">Tambah ke Keranjang</div>') ?>
+          <?php echo esc_url('Dashboard/detail/'.$brg->id_barang, '<div class="btn btn-sm btn-success mt-3">Detail</div>') ?>
         </div>
       </div>
     <?php endforeach; ?>
